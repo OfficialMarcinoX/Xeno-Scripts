bc252b41efab87bb75c724bc959803a773f46cc7
OfficialMarcinoX <marcindklim@gmail.com>
2026-03-25 13:55:04 +0100

    Replace OrionLib with Rayfield for GUI

--- fly.lua
+++ fly.lua
@@ -1,18 +1,37 @@
--- Pobranie biblioteki GUI (Tego Ci brakowało!)
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Xeno Fly Menu", HidePremium = false, SaveConfig = true, ConfigFolder = "XenoConfig"})
+-- Ładowanie nowoczesnej biblioteki Rayfield
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
--- Główne zmienne (Tego też brakowało!)
-local Flying = false
-local Speed = 50
+-- Główne okno Twojego Huba
+local Window = Rayfield:CreateWindow({
+   Name = "planexd_0 Script Hub",
+   LoadingTitle = "Ładowanie skryptów...",
+   LoadingSubtitle = "by planexd_0",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "MojeSkrypty",
+      FileName = "HubConfig"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "brak",
+      RememberJoins = true
+   },
+   KeySystem = false -- Na razie bez hasła
+})
+
+-- Zmienne do latania
 local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
+local Flying = false
+local Speed = 50
 local bv
 
 -- Funkcja latania
 local function toggleFly()
     local character = player.Character
+    if not character then return end
     local root = character:FindFirstChild("HumanoidRootPart")
+    if not root then return end
     
     if Flying then
         bv = Instance.new("BodyVelocity")
@@ -32,43 +51,49 @@ local function toggleFly()
     end
 end
 
--- Zakładka w Menu
-local Tab = Window:MakeTab({
-	Name = "Główne",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+-- Tworzenie zakładki
+local TabUniversal = Window:CreateTab("Universal", 4483362458) -- 4483362458 to ID ikonki globu
+
+-- Dodanie sekcji w zakładce
+local Section = TabUniversal:CreateSection("Opcje Poruszania Się")
+
+-- Przycisk (Toggle) włączający latanie
+local FlyToggle = TabUniversal:CreateToggle({
+   Name = "Latanie (Zastępuje też klawisz F)",
+   CurrentValue = false,
+   Flag = "FlyToggle",
+   Callback = function(Value)
+        Flying = Value
+        toggleFly()
+   end,
 })
 
-Tab:AddToggle({
-	Name = "Latanie (Klawisz F)",
-	Default = false,
-	Callback = function(Value)
-		Flying = Value
-		toggleFly()
-	end    
+-- Suwak (Slider) do prędkości
+local FlySlider = TabUniversal:CreateSlider({
+   Name = "Prędkość latania",
+   Range = {10, 300},
+   Increment = 1,
+   Suffix = "Speed",
+   CurrentValue = 50,
+   Flag = "FlySpeed",
+   Callback = function(Value)
+        Speed = Value
+   end,
 })
 
-Tab:AddSlider({
-	Name = "Prędkość",
-	Min = 10,
-	Max = 300,
-	Default = 50,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "Szybkość",
-	Callback = function(Value)
-		Speed = Value
-	end    
-})
-
--- Obsługa klawisza F
+-- Obsługa klawisza F z aktualizacją przycisku w menu
 mouse.KeyDown:Connect(function(key)
     if key:lower() == "f" then
         Flying = not Flying
-        OrionLib:MakeNotification({Name = "Xeno Fly", Content = "Latanie: " .. (Flying and "WŁĄCZONE" or "WYŁĄCZONE"), Time = 2})
+        FlyToggle:Set(Flying) -- To automatycznie przesuwa przycisk w menu!
         toggleFly()
     end
 end)
 
--- Odpalenie GUI na ekranie
-OrionLib:Init()
+-- Komunikat na start
+Rayfield:Notify({
+   Title = "Gotowe!",
+   Content = "Witaj w planexd_0 Script Hub. Miłej zabawy!",
+   Duration = 5,
+   Image = 4483362458,
+})
